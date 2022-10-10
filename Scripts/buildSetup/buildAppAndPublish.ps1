986b6e7c78fb3b5472924c4b3535199bb980503d
Jürgen Mayrbäurl <jmayrbaeurl@live.at>
2022-10-10 12:00:59 +0200

    Fixed hard code resource group

--- Scripts/buildSetup/buildAppAndPublish.ps1
+++ Scripts/buildSetup/buildAppAndPublish.ps1
@@ -6,7 +6,7 @@ param (
 Write-Output ('Deploying Azure resources to build artefacts for the TTTech Nerve Digital twin demo ' + $rg)
 az deployment group create --verbose --resource-group $rg --template-file .\build.bicep --confirm-with-what-if
 
-$regName=$(az acr list -g 'tttech-work-rg' --query '[0].{Name:name}' | ConvertFrom-Json)
+$regName=$(az acr list -g $rg --query '[0].{Name:name}' | ConvertFrom-Json)
 az acr build -f ..\..\adtinoutfunctions\Dockerfile --registry $regName.Name --resource-group $rg --image $imagename ..\..\adtinoutfunctions\
 
 Write-Output ('Image can be pulled from: ' + $regName + "azurecr.io" + $imagename)
