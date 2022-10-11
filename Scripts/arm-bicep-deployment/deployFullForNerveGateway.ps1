a259fc7726494f649926758ee0fdf98a935d7c14
Jürgen Mayrbäurl <jmayrbaeurl@live.at>
2022-10-11 11:11:35 +0200

    Fixed issues with hard coded resource groups

--- Scripts/arm-bicep-deployment/deployFullForNerveGateway.ps1
+++ Scripts/arm-bicep-deployment/deployFullForNerveGateway.ps1
@@ -1,5 +1,6 @@
 param (
 	[string] $rg = 'tttech-nervedemo-rg',
+	[string] $deviceId = 'NerveGateway',
 	[string] $dockerserverurl
 )
 
@@ -8,8 +9,8 @@ $principalId = az ad signed-in-user show --query 'id' | ConvertFrom-Json
 .\deployAzureServices -rg $rg -principalId $principalId -inputMsgFormat 'NerveGateway' -dockerserverurl $dockerserverurl
 
 Write-Output 'Now setting up the Digital Twin instance'
-$dtName=$(az dt list -g tttech-nervedemo-rg --query '[0].{Name:name}' | ConvertFrom-Json)
+$dtName=$(az dt list -g $rg --query '[0].{Name:name}' | ConvertFrom-Json)
 .\createADTGraphForNerveGateway.ps1 -rg $rg -dtName $dtName.Name
 
 Write-Output 'Now registering IoT Hub device'
-.\registerIoTHubDevice.ps1 -rg $rg
+.\registerIoTHubDevice.ps1 -rg $rg -deviceId $deviceId
